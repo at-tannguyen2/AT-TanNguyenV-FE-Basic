fb652c39abd0e3340ee5212ed17964c1dee36827
vantannguyen1556 <54669467+vantannguyen1556@users.noreply.github.com>
2020-02-06 18:31:02 +0700

    fix classname, fix convention, fix duplicacted code

--- On tap Js-2/comment.js
+++ On tap Js-2/comment.js
@@ -1,57 +1,51 @@
+var listComment;
+//get comment data from Local Storage
+function getLocalStorageComments() {
+  listComment = localStorage.getItem('listComment');
+  listComment = listComment ? JSON.parse(listComment) : [];
+}
+
+//save comment data to Local Storage
+function setLocalStorageComment() {
+  localStorage.setItem('listComment', JSON.stringify(listComment));
+}
+
 //push comment to array
-function pushToArr() {
+function pushCommentToArr() {
   var btnAdd = document.getElementsByClassName('btn-add')[0];
-  btnAdd.addEventListener('click', function () {
-    var listComment = localStorage.getItem('listComment');
-    listComment = listComment ? JSON.parse(listComment) : [];
-    var commentValue = document.getElementById('js-value').value;
-    var id = 0;
-    var objCmt = {
-      content: commentValue,
-      id: id,
-      idArtical: 1,
-      isDelete: false,
-      user: {
-        avatar: "https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/1412243/910/607/m1/fpnw/wm0/lawyer-avatar-flat-icon-01-.jpg?1467280299&s=7f76fc556d9c3ce29567ee4f325dd1f3",
-        id: 1,
-        name: "Tan Nguyen V."
-      }
-    }
-    if (listComment) {
-      if (listComment.length >= 1) {
-        for (var i = 0; i < listComment.length; i++) {
-          var objCmt = {
-            content: commentValue,
-            id: listComment.length,
-            idArtical: 1,
-            isDelete: false,
-            user: {
-              avatar: "https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/1412243/910/607/m1/fpnw/wm0/lawyer-avatar-flat-icon-01-.jpg?1467280299&s=7f76fc556d9c3ce29567ee4f325dd1f3",
-              id: 1,
-              name: "Tan Nguyen V."
-            }
-          }
-        }
-        listComment.push(objCmt);
-      } else {
-        listComment.push(objCmt);
-      }
+  btnAdd.addEventListener('click', function() {
+    getLocalStorageComments();
+    var commentValue = document.getElementById('js-comment-value').value;
+    if (commentValue === '') {
+      alert('Please add a comment...!!!')
     } else {
+      var id = 0;
+      var objCmt = {
+        content: commentValue,
+        id: listComment.length && listComment ? listComment.length : 0,
+        idArticle: 1,
+        isDelete: false,
+        user: {
+          avatar: "https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/1412243/910/607/m1/fpnw/wm0/lawyer-avatar-flat-icon-01-.jpg?1467280299&s=7f76fc556d9c3ce29567ee4f325dd1f3",
+          id: 1,
+          name: "Tan Nguyen V."
+        }
+      }
       listComment.push(objCmt);
+      setLocalStorageComment();
+      renderComment();
+      document.getElementById('js-comment-value').value = '';
     }
-    localStorage.setItem('listComment', JSON.stringify(listComment));
-    renderComment();
-    document.getElementById('js-value').value = '';
   });
 }
 
 //render comment to HTML
 function renderComment() {
   var resultComment = document.getElementById('js-comment');
-  var listComment = JSON.parse(localStorage.getItem('listComment'));
-  var content = listComment.map(function (item, index) {
-    if (item.isDelete === false) {
-      return '<div class="comment-result"><div class="comment-result-title"><img class="img-comment" src="' + item.user.avatar + '"><p class="bold">' + item.user.name + '</p></div><div class="comment-result-content"><p class="comment-text">' + item.content + '</p><button class="btn-delete bold" data-id="' + item.id + '"><i class="far fa-trash-alt"></i></button></div></div>';
+  getLocalStorageComments();
+  var content = listComment.map(function(item, index) {
+    if (!item.isDelete) {
+      return '<div class="comment-result"><div class="comment-result-article"><img class="img-comment" src="' + item.user.avatar + '"><p class="bold">' + item.user.name + '</p></div><div class="comment-result-content d-flex align-item-center"><p class="comment-text">' + item.content + '</p><button class="btn-delete bold" data-id="' + item.id + '"><i class="far fa-trash-alt"></i></button></div></div>';
     }
   });
   resultComment.innerHTML = content.join('');
@@ -62,21 +56,20 @@ function renderComment() {
 function deleteComment() {
   var btnDelete = document.getElementsByClassName('btn-delete');
   for (var i = 0; i < btnDelete.length; i++) {
-    btnDelete[i].addEventListener('click', function (event) {
+    btnDelete[i].addEventListener('click', function(event) {
       var id = +event.target.dataset.id;
-      var listComment = localStorage.getItem('listComment');
-      listComment = listComment ? JSON.parse(listComment) : [];
+      getLocalStorageComments();
       for (var i = 0; i < listComment.length; i++) {
         if (listComment[i].id === id) {
           listComment[i].isDelete = true;
         }
       }
       alert('Comment delete successfully!!!');
-      localStorage.setItem('listComment', JSON.stringify(listComment));
+      setLocalStorageComment();
       renderComment();
     })
   }
 }
 
-pushToArr();
+pushCommentToArr();
 renderComment();
--- On tap Js-2/news.js
+++ On tap Js-2/news.js
@@ -1,14 +1,28 @@
+var listNews;
+function getLocalStorageNews() {
+  listNews = localStorage.getItem('listNews');
+  listNews = listNews ? JSON.parse(listNews) : [];
+}
+
+function setLocalStorageNews() {
+  localStorage.setItem('listNews', JSON.stringify(listNews));
+}
+
 var listNews = [
-  { id: '1', title: 'Bệnh nhân thứ 9 nhiễm virus corona tại Việt Nam', content: 'Bệnh nhân mới có kết quả dương tính với virus corona là ông T.C.P., 30 tuổi, địa chỉ xã Minh Quang, huyện Tam Đảo, tỉnh Vĩnh Phúc. Đây cũng là nhân viên Công ty TNHH Nihon Plast của Nhật Bản được cử sang Trung Quốc tập huấn tại Thành phố Vũ Hán, tỉnh Hồ Bắc, Trung Quốc.', isDelete: false }
+  {
+    id: '1',
+    title: 'Bệnh nhân thứ 9 nhiễm virus corona tại Việt Nam',
+    content: 'Bệnh nhân mới có kết quả dương tính với virus corona là ông T.C.P., 30 tuổi, địa chỉ xã Minh Quang, huyện Tam Đảo, tỉnh Vĩnh Phúc. Đây cũng là nhân viên Công ty TNHH Nihon Plast của Nhật Bản được cử sang Trung Quốc tập huấn tại Thành phố Vũ Hán, tỉnh Hồ Bắc, Trung Quốc.',
+    isDelete: false
+  }
 ];
-localStorage.setItem('listNews', JSON.stringify(listNews));
+setLocalStorageNews();
 
 function renderNews() {
-  var news = document.getElementsByClassName('news')[0];
-  var listNews = localStorage.getItem('listNews');
-  listNews = listNews ? JSON.parse(listNews) : [];
-  var content = listNews.map(function (item) {
-    return '<div class="news-title"><h1 class="title text-upscase bold text-center">' + item.title + '</h1></div><div class="news-text"><p>' + item.content + '</p></div>';
+  var news = document.getElementsByClassName('js-news')[0];
+  getLocalStorageNews();
+  var content = listNews.map(function(item) {
+    return '<div class="news-title"><h1 class="title text-primary text-upscase bold text-center">' + item.title + '</h1></div><div class="news-text"><p>' + item.content + '</p></div>';
   });
   news.innerHTML = content.join('');
 }
